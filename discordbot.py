ff6ca75f5f08ec955310afe0034c38aeb740684f
PP <96706159+PP-darekano@users.noreply.github.com>
2021-12-27 11:10:20 +0900

    about

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,10 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command()
+async def rb(ctx):
+    await ctx.send('RB-BOT')
+
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
